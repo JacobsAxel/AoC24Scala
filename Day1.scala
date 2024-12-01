6bc2ad44b5c1e1ef33d54a2fd45c603c7ee851de
JacobsAxel <100530265+JacobsAxel@users.noreply.github.com>
2024-12-01 19:48:17 +0100

    layout cleanup

--- Day1.scala
+++ Day1.scala
@@ -1,31 +1,33 @@
 import scala.io.Source
 
 @main def day1(): Unit =
-
-  println(result1)
-  println(result2)
+  val (left,right) = parse("1")
+  println(part1(left,right))
+  println(part2(left,right))
 
 //Part1
 
-val lines = Source.fromFile("src/input/1").getLines()
-val (left,right) = lines.map( x => 
-  val pair = x.split("   ").map(_.toInt)
-  (pair(0),pair(1))
+def parse(file: String): (List[Int],List[Int]) =
+  val lines = Source.fromFile("src/input/" ++ file).getLines()
+  val (left,right) = lines.map( x => 
+    val pair = x.split("   ").map(_.toInt)
+    (pair(0),pair(1))
   ).toList.unzip
+  (left,right)
 
-val (sortedL,sortedR) = (left.sorted,right.sorted)
-  
-val distances = sortedL.zip(sortedR).map((x,y) => math.abs(x-y))
-
-val result1 = distances.sum
+def part1(left:List[Int],right:List[Int]): Int = 
+  val (sortedL,sortedR) = (left.sorted,right.sorted)
+  val distances = sortedL.zip(sortedR).map((x,y) => math.abs(x-y))
+  val result1 = distances.sum
+  result1
 
 //Part2
 
-val hashMap = right.groupBy(identity).mapValues(_.size)
-
-val countProduct = left.map(x => hashMap.contains(x) match
-    case true => x * hashMap(x)
-    case false => 0
-   ) 
-
-val result2 = countProduct.sum
+def part2(left:List[Int],right:List[Int])=
+  val hashMap = right.groupBy(identity).mapValues(_.size)
+  val countProduct = left.map(x => hashMap.contains(x) match
+      case true => x * hashMap(x)
+      case false => 0
+    ) 
+  val result2 = countProduct.sum
+  result2
